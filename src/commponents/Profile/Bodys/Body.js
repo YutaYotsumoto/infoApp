37829f0019230061be6701cecaa591f8d4963a9d
Yuta Yotsumoto <y.2000529@gmail.com>
2021-06-18 10:38:19 +0900

    FlatList内コンポネント画面内に収める ＝＞height:100% marginBottom

--- src/commponents/Profile/Bodys/Body.js
+++ src/commponents/Profile/Bodys/Body.js
@@ -4,9 +4,9 @@ import {View, Text, StyleSheet} from 'react-native';
 const Body = ({id, city, street}) => {
   return (
     <View>
-      <Text style={styles.font}>id: {id}</Text>
-      <Text style={styles.font}>city: {city}</Text>
-      <Text style={styles.font}>street: {street}</Text>
+      <Text style={styles.font}>id : {id}</Text>
+      <Text style={styles.font}>city : {city}</Text>
+      <Text style={styles.font}>street : {street}</Text>
     </View>
   );
 };
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   font: {
     fontSize: 15,
     fontStyle: 'italic',
-    fontWeight: 'bold',
+    fontWeight: '400',
     color: 'black',
     margin: 5,
   },
